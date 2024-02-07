f2cd58f229e4f48f0ad4c0e7f560f06b37814fae
Rain-Gayming <raingayming@gmail.com>
2024-02-07 21:32:37 +0000

    Base

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+mod player;
+use player::PlayerPlugin;
+
+mod camera;
+use camera::CameraPlugin;
+
+mod world;
+use world::WorldPlugin;
+
+
+fn main(){
+    App::new().add_plugins((DefaultPlugins, PlayerPlugin, CameraPlugin, WorldPlugin)).run();
+}
